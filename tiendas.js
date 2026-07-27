ac303cfbd5e083a91e23c7571a4adc32faaf05e1
nelsonpro-code <nelsonmusic1982@gmail.com>
2026-07-26 23:56:57 -0400

    Subida de archivos JS del juego

---
+++ tiendas.js
@@ -0,0 +1,29 @@
+window.DATOS_TIENDAS = [
+  {
+    "id": 1,
+    "nombre": "Comercial Viacha Tech",
+    "categoria": "Electrónica y Celulares",
+    "lat": -16.6533,
+    "lng": -68.3017,
+    "whatsapp": "59170000000",
+    "descripcion": "Venta de accesorios para celular y reparación."
+  },
+  {
+    "id": 2,
+    "nombre": "Boutique Moda Viacha",
+    "categoria": "Ropa y Calzado",
+    "lat": -16.6515,
+    "lng": -68.2995,
+    "whatsapp": "59170000001",
+    "descripcion": "Ropa variada para damas y varones."
+  },
+  {
+    "id": 3,
+    "nombre": "Nelson Tech & Software",
+    "categoria": "Desarrollo de Software y Tecnología",
+    "lat": -16.657764,
+    "lng": -68.30211,
+    "whatsapp": "59163159223",
+    "descripcion": "Desarrollo de software y tecnología en PC y Android."
+  }
+];
